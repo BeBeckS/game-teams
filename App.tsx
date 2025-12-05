6d53feefc8249dcd029d56fd1d232cd0b53f5524
BeBeckS <be.beck.silva200327@gmail.com>
2025-12-04 22:57:50 -0300

    Update Files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Platform, StatusBar, View} from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components/native'; 
+import Constants from 'expo-constants';
+
+
+import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
+import { NewGroup } from '@screens/NewGroup';
+
+
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  const containerStyle = {
+    flex: 1,
+    backgroundColor: theme.COLORS.GRAY_600,
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 12,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>OLÁ MUNDO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <View style={containerStyle}>
+          { fontsLoaded ? <Players /> : <Loading /> }
+        </View>
+      </>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
